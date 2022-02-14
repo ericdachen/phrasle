94cac2e17254ba8290a094c42dad1dbfd9d2c8c0
ericdachen <e56chen@uwaterloo.ca>
2022-02-13 23:24:36 -0500

    Letters change when you click

--- phrasle/src/App.js
+++ phrasle/src/App.js
@@ -1,7 +1,5 @@
 
 import './App.css';
-import './components/keyboard/keyboard'
-import KeyBoard from './components/keyboard/keyboard';
 import PhraseBoard from './components/word/phrase/phrase'
 import Interface from './components/interface/interface';
 import styled, {css} from 'styled-components'
