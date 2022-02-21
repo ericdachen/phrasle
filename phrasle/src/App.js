dd4ef2f7f48bfd1877df8ea5d80634c2a7b7ea41
Wen Zhang <wz2705@gmail.com>
2022-02-20 21:45:50 -0500

    create statistics modal

--- phrasle/src/App.js
+++ phrasle/src/App.js
@@ -1,12 +1,12 @@
-
 import './App.css';
-import PhraseBoard from './components/word/phrase/phrase'
+import NavBar from './components/navbar/navbar';
 import Interface from './components/interface/interface';
-import styled, {css} from 'styled-components'
+import styled, {css} from 'styled-components';
 
 function App() {
   return (
     <div>
+      <NavBar/>
       <Interface/>
     </div>
   );
